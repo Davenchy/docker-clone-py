5fb37e25a073e1094b708fa295b741658a5d8fbf
Fadi Asaad <firon1222@gmail.com>
2023-12-21 02:33:33 +0200

    pass 4th stage: Filesystem Isolation

--- app/main.py
+++ app/main.py
@@ -1,20 +1,34 @@
 import subprocess
+import tempfile
+import shutil
 import sys
+import os
 
 
-def main():
-    commandIn = sys.argv[3]
-    argsIn = sys.argv[4:]
+def run(command, *args):
+    tempRootPath = tempfile.mkdtemp()
+    commandFileName = os.path.basename(command)
+    commandPath = os.path.join('/', commandFileName)
 
-    command = [commandIn, *argsIn]
+    shutil.copy2(command, tempRootPath)
+    os.chroot(tempRootPath)
+
     process = subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        [commandPath, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
+
     print(stdout.decode("utf-8"), end="")
     print(stderr.decode("utf-8"), file=sys.stderr, end="")
 
     code = process.wait()
     sys.exit(code)
+
+
+def main():
+    command = sys.argv[3]
+    args = sys.argv[4:]
+
+    run(command, *args)
 
 
 if __name__ == "__main__":
