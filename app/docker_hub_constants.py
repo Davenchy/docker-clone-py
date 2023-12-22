52aef53c70a295ff17cf52a7aaff0097e9acf04a
Fadi Asaad <firon1222@gmail.com>
2023-12-22 02:59:33 +0200

    define docker hub endpoints

---
+++ app/docker_hub_constants.py
@@ -0,0 +1,5 @@
+TOKEN_ENDPOINT = "https://auth.docker.io/token?service=registry.docker.io\
+&scope=repository:{}:pull"
+BASE_ENDPOINT = "https://registry.hub.docker.com/v2"
+MANIFESTS_ENDPOINT = "{}/manifests/{}"
+BLOB_ENDPOINT = "{}/blobs/{}"
