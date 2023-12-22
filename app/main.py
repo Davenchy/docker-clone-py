3cae871f779be374d4d31c6f710573e4ac59ca7e
Fadi Asaad <firon1222@gmail.com>
2023-12-22 03:03:20 +0200

    dummy entry point for my docker clone, this for testing and it is full of bugs XD

--- app/main.py
+++ app/main.py
@@ -1,42 +1,88 @@
-import subprocess
-import tempfile
-import shutil
-import ctypes
-import sys
-import os
+import argparse
+from os import getcwd
+
+from app.docker_image import DockerImage, DockerImageDownloader
+from app.utils import format_size, format_time
+
+parser = argparse.ArgumentParser()
+subparser = parser.add_subparsers()
+
+pull_parser = subparser.add_parser(
+    'pull',
+    help="pull docker image from docker hub",
+)
+
+pull_parser.add_argument(
+    'image_name', type=str,
+    help="""image name to pull.\n
+    Examples:
+        library/ubuntu:latest OR library/ubuntu OR ubuntu OR ubuntu:latest""",
+)
+
+pull_parser.add_argument(
+    "path", type=str, help="path to save downloaded image",
+)
+
+create_parser = subparser.add_parser(
+    'create',
+    help="create container from downloaded docker image"
+)
+
+create_parser.add_argument(
+    'image', type=str, help="path to downloaded image",
+)
+
+create_parser.add_argument(
+    'container_path', type=str, help='path to container filesystem/root',
+)
+
+exec_parser = subparser.add_parser(
+    'exec',
+    help="run container and execute command",
+)
+
+exec_parser.add_argument(
+    "container_path", type=str,
+    help="path to created container filesystem/root",
+)
+
+exec_parser.add_argument(
+    "command", type=str, help="command to execute in container",
+)
+
+args = parser.parse_args()
 
 
-CLONE_NEWPID = 0x20000000
+def on_download_started(downloader: DockerImageDownloader):
+    print('download started:', downloader.filename)
 
 
-def run(command, *args):
-    tempRootPath = tempfile.mkdtemp()
-    commandFileName = os.path.basename(command)
-    commandPath = os.path.join('/', commandFileName)
-
-    shutil.copy2(command, tempRootPath)
-    os.chroot(tempRootPath)
-
-    libc = ctypes.CDLL(None)
-    libc.unshare(CLONE_NEWPID)
-
-    process = subprocess.Popen(
-        [commandPath, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-
-    print(stdout.decode("utf-8"), end="")
-    print(stderr.decode("utf-8"), file=sys.stderr, end="")
-
-    code = process.wait()
-    sys.exit(code)
+def on_download_completed(downloader: DockerImageDownloader):
+    print('download completed:', downloader.filename)
 
 
-def main():
-    command = sys.argv[3]
-    args = sys.argv[4:]
+def on_download_updated(downloader: DockerImageDownloader):
+    msg = '({:.2f}%), Size: {}, Downloaded: {}, Speed: {}/s, \
+    Estimated Time: {}'
+    print(msg.format(
+        downloader.progress * 100,
+        format_size(downloader.total_size),
+        format_size(downloader.downloaded),
+        format_size(downloader.speed),
+        format_time(downloader.estimated_time_remaining),
+    ))
 
-    run(command, *args)
 
+if __name__ == '__main__':
+    dockerImage = DockerImage.from_image_name('library/ubuntu:latest')
+    download_profile = dockerImage.create_download_profile('arm64')
+    downloader = download_profile.create_downloader(getcwd())
 
-if __name__ == "__main__":
-    main()
+    downloader.started.connect(on_download_started)
+    downloader.updated.connect(on_download_updated)
+    downloader.completed.connect(on_download_completed)
+
+    downloader.start()
+
+    print(f'Name: {downloader.filepath}')
+    print(f'Size: {download_profile.size_human_readable}')
